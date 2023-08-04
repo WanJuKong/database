9b9df1bae7bdd1acda381723f1c6d3b3dca2c843
WanJuKong <juwan3057@naver.com>
2023-08-04 19:39:57 +0900

    08/04(19:39)

--- auto_git.c
+++ auto_git.c
@@ -6,9 +6,26 @@ int main(void) {
 	time_t Time = time(NULL);
 	struct tm* t = localtime(&Time);
 	char commit[100];
-	system("git add *");
+	if(system("git add *") == 0){
+		system("git status");
+		printf("\nare you sure you want to commit these?(y/n)");
+		fflush(stdin);
+		if(getchar() != 'y'){
+			printf("[ Error : commit denied ]");
+			return 0;
+		}
+	} else {
+	       printf("[ ERROR : error caused while adding ]");
+	       return 0;
+	}
 	snprintf(commit, sizeof(commit), "git commit -m '%02d/%02d(%02d:%02d)'", t->tm_mon+1, t->tm_mday, t->tm_hour, t->tm_min);
-	system(commit);
-	system("git push origin main");
-	printf("data pushed successfully\n");
+	if(system(commit) != 0){
+		printf("[ ERROR : error caused while commiting ]");
+		return 0;
+	}
+	if(system("git push origin main") != 0){
+		printf("[ ERROR : error caused while commiting ]");
+		return 0;
+	}
+	printf("[ data pushed successfully ]\n");
 }
---
+++ copy.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void){
+	const char* copyFrom = "/var/www/html/sql";
+	const char* copyTo = ".";
+	char copy[100];
+	snprintf(copy, sizeof(copy), "cp -r %s/* %s", copyFrom, copyTo);
+	int result = system(copy);
+	if(result)
+		printf("[ files copied successfully ]\n");
+	else
+		printf("[ ERROR : error code %d ]\n", result);
+}
